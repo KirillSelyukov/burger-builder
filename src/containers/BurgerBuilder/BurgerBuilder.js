f0eef6f08dd03198c50d4dd4051b4093d825cf80
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-03-07 19:52:38 +0200

    Inject BurgerBuilder to App

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,13 @@
 import React,{Component} from 'react';
+import { Aux } from '../../hoc/Auxiliary';
 
 export class BurgerBuilder extends Component{
-
+    render(){
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
 }
