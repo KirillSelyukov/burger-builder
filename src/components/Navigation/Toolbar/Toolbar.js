74535cdd608ad7cb9a3551062d9ebb21cbb9ff4e
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-03-14 19:29:15 +0200

    Use Logo in menu

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Toolbar.css';
+import { Logo } from '../../Logo/Logo';
 
 export const Toolbar = (props) => (
     <header className='Toolbar'>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
