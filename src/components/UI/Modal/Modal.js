933bb8df9bf5f468f55eed0c73200438a174fc13
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-03-14 10:45:42 +0200

    Creating the Order Summary Modal

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Modal.css';
+
+export const Modal = (props) => (
+    <div className='Modal'>
+        {props.children}
+    </div>
+);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Aux } from '../../hoc/Auxiliary';
-import { Burger } from '../../components/Burger/Burger';
+import { Burger } from '../../components/Burger';
 import { BuildControls } from '../../components/Burger/BuildControls';
+import {Modal} from '../../components/UI/Modal';
+import { OrederSummary } from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIET_PRICES = {
     salad: 0.5,
@@ -68,6 +70,9 @@ export class BurgerBuilder extends Component {
         return (
 
             <Aux>
+                <Modal>
+                    <OrederSummary ingredients ={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
