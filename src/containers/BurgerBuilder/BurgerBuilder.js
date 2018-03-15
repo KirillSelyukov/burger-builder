a30514e046a76813ee02355fb8d504a52f39052c
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-03-15 17:09:56 +0200

    Send POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import { Aux } from '../../hoc/Auxiliary';
-import { Burger } from '../../components/Burger';
-import { BuildControls } from '../../components/Burger/BuildControls';
-import { Modal } from '../../components/UI/Modal';
-import { OrederSummary } from '../../components/Burger/OrderSummary/OrderSummary';
+import React, { Component } from 'react'
+
+import axios from '../../axios-orders'
+import { Aux } from '../../hoc/Auxiliary'
+import { Burger } from '../../components/Burger'
+import { BuildControls } from '../../components/Burger/BuildControls'
+import { Modal } from '../../components/UI/Modal'
+import { OrederSummary } from '../../components/Burger/OrderSummary/OrderSummary'
 
 const INGREDIET_PRICES = {
     salad: 0.5,
@@ -24,53 +26,72 @@ export class BurgerBuilder extends Component {
         purchasable: false,
         purchasing: false,
 
-    };
+    }
 
     updatePurchaseState = (ingredients) => {
         const sum = Object.keys(ingredients)
             .map(igKey => {
-                return ingredients[igKey];
+                return ingredients[igKey]
             })
-            .reduce((sum, el) => { return sum + el; }, 0);
+            .reduce((sum, el) => { return sum + el }, 0)
         this.setState({ purchasable: sum > 0 })
     }
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type]
-        const updatedCount = oldCount + 1;
-        const updatedIngredients = { ...this.state.ingredients };
-        updatedIngredients[type] = updatedCount;
-        const priceAddition = INGREDIET_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice + priceAddition;
-        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-        this.updatePurchaseState(updatedIngredients);
+        const updatedCount = oldCount + 1
+        const updatedIngredients = { ...this.state.ingredients }
+        updatedIngredients[type] = updatedCount
+        const priceAddition = INGREDIET_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+        this.updatePurchaseState(updatedIngredients)
     }
 
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type]
         if (oldCount <= 0) {
-            return;
+            return
         }
-        const updatedCount = oldCount - 1;
-        const updatedIngredients = { ...this.state.ingredients };
-        updatedIngredients[type] = updatedCount;
-        const priceDeduction = INGREDIET_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice - priceDeduction;
-        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-        this.updatePurchaseState(updatedIngredients);
+        const updatedCount = oldCount - 1
+        const updatedIngredients = { ...this.state.ingredients }
+        updatedIngredients[type] = updatedCount
+        const priceDeduction = INGREDIET_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice - priceDeduction
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+        this.updatePurchaseState(updatedIngredients)
     }
 
     purchaseHandler = () => {
-        this.setState({ purchasing: true });
+        this.setState({ purchasing: true })
     }
 
     purchaseCancelHandler = () => {
-        this.setState({ purchasing: false });
+        this.setState({ purchasing: false })
     }
-    purchaseContinueHandler() {
-        alert('You Continue!');
+
+    purchaseContinueHandler = () => {
+        //alert('You Continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Max Max',
+                address: {
+                    street: 'Test street',
+                    zipCode: '123124',
+                    country: 'USA',
+                },
+                email: 'someemail@someemail.com'
+            },
+            deliveryMethod: 'ASAP'
+        };
+
+        axios.post('/orders.json', order)
+        .then(response=>console.log(response))
+        .catch(error =>console.log(error));
     }
 
     render() {
@@ -79,7 +100,7 @@ export class BurgerBuilder extends Component {
         }
 
         for (let key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <= 0;
+            disabledInfo[key] = disabledInfo[key] <= 0
         }
 
         return (
@@ -101,6 +122,6 @@ export class BurgerBuilder extends Component {
                     ordered={this.purchaseHandler}
                 />
             </Aux>
-        );
+        )
     }
 };
