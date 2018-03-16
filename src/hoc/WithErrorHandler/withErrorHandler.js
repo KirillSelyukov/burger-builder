a5945a17d9df5ef9cd15d4359a454b64eb3533ad
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-03-16 13:16:14 +0200

    Removу Old Interceptors

--- src/hoc/WithErrorHandler/withErrorHandler.js
+++ src/hoc/WithErrorHandler/withErrorHandler.js
@@ -4,19 +4,22 @@ import { Aux } from '../Auxiliary';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
-
         state = {
             error: null
         };
 
         componentWillMount() {
-            axios.interceptors.request.use(request => {
+            this.reqInteceptor = axios.interceptors.request.use(request => {
                 this.setState({ error: null });
                 return request;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({ error: error });
             });
+        }
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInteceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
