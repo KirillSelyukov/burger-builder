55119c30559645f501f1c2818f014d770c51c03e
Kirill Selyukov <kselukov@gmail.com>
2018-05-03 16:12:56 +0300

    Add redux web tools to the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import App from './App';
+import './index.css';
 import reducer from './store/reducer';
-const store = createStore(reducer);
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
@@ -18,4 +18,3 @@ const app = (
     </Provider>
 )
 ReactDOM.render(app, document.getElementById('root'));
-registerServiceWorker();
