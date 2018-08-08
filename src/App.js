dab58acf3b158a5c13290162277fa68fa5ff4a9e
Kirill Selyukov <kselukov@gmail.com>
2018-08-08 18:24:11 +0300

    Add lazy loading for some component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import { connect } from 'react-redux';
 
 import { Layout } from './containers/Layout';
 import { BurgerBuilder } from './containers/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+// import Checkout from './containers/Checkout/Checkout';
+// import Orders from './containers/Orders/Orders';
+// import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
   componentDidMount() {
@@ -17,7 +28,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path='/auth' exact component={Auth} />
+        <Route path='/auth' exact component={asyncAuth} />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
@@ -26,9 +37,9 @@ class App extends Component {
       routes = (
         <Switch>
           <Route path='/' exact component={BurgerBuilder} />
-          <Route path='/auth' exact component={Auth} />
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' exact component={Orders} />
+          <Route path='/auth' exact component={asyncAuth} />
+          <Route path='/checkout' component={asyncCheckout} />
+          <Route path='/orders' exact component={asyncOrders} />
           <Route path='/logout' exact component={Logout} />
           <Redirect to='/' />
         </Switch>
