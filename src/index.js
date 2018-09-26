328fb6d075b5dfab58de806e5b5bff36c26f62a4
Kirill Selyukov <kselukov@gmail.com>
2018-09-26 18:12:48 +0300

    Hook the saga up

--- src/index.js
+++ src/index.js
@@ -4,28 +4,34 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import App from './App';
 import './index.css';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderRuducer from './store/reducers/order';
 import authRuducer from './store/reducers/auth';
+import { logoutSaga } from './sagas/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer = combineReducers({
-    burgerBuilder: burgerBuilderReducer,
-    order: orderRuducer,
-    auth: authRuducer
+  burgerBuilder: burgerBuilderReducer,
+  order: orderRuducer,
+  auth: authRuducer
 });
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
+);
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-
+sagaMiddleware.run(logoutSaga);
 const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 ReactDOM.render(app, document.getElementById('root'));
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -2,93 +2,101 @@ import * as actionTypes from '../../store/actions/actions';
 import axios from 'axios';
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    };
-}
-
-export const authSuccess = (idToken, localId) => {
-    return {
-        type: actionTypes.AUTH_START_SUCCESS,
-        token: idToken,
-        userId: localId
-    };
-}
-
-export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_START_FAIL,
-        error: error
-    };
-}
-
-export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCb5CTQi5_vI_D8T3tlGhxDgO74gBnmfyc'
-        if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCb5CTQi5_vI_D8T3tlGhxDgO74gBnmfyc';
-        }
-        axios.post(url, authData)
-            .then(res => {
-                const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000);
-                localStorage.setItem('token', res.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', res.data.localId);
-                dispatch(authSuccess(res.data.idToken, res.data.localId));
-                dispatch(checkAuthTimeout(res.data.expiresIn));
-            })
-            .catch(error => {
-                dispatch(authFail(error.response.data.error));
-            });
-    }
-}
-
-export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
-    };
-}
-
-export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
-
-    return {
-        type: actionTypes.AUTH_LOGOUT
-    };
+  return {
+    type: actionTypes.AUTH_START
+  };
 };
 
-export const setAuthRedirectPath = (path) => {
-    return {
-        type: actionTypes.SET_AUTH_REDIRECT_PATH,
-        path: path
+export const authSuccess = (idToken, localId) => {
+  return {
+    type: actionTypes.AUTH_START_SUCCESS,
+    token: idToken,
+    userId: localId
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_START_FAIL,
+    error: error
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url =
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCb5CTQi5_vI_D8T3tlGhxDgO74gBnmfyc';
+    if (!isSignup) {
+      url =
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCb5CTQi5_vI_D8T3tlGhxDgO74gBnmfyc';
     }
-}
+    axios
+      .post(url, authData)
+      .then(res => {
+        const expirationDate = new Date(
+          new Date().getTime() + res.data.expiresIn * 1000
+        );
+        localStorage.setItem('token', res.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', res.data.localId);
+        dispatch(authSuccess(res.data.idToken, res.data.localId));
+        dispatch(checkAuthTimeout(res.data.expiresIn));
+      })
+      .catch(error => {
+        dispatch(authFail(error.response.data.error));
+      });
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
+export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
+
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const setAuthRedirectPath = path => {
+  return {
+    type: actionTypes.SET_AUTH_REDIRECT_PATH,
+    path: path
+  };
+};
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            dispatch(logout());
-        } else {
-            const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if (expirationDate > new Date()) {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000))
-            } else {
-                dispatch(logout());
-            }
-        }
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      //dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate > new Date()) {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      } else {
+        // dispatch(logout());
+      }
     }
-
-}
+  };
+};
